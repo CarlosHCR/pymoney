6439b1408c59664f4f66a4ac7458418214677a0f
carlos <carloshrc5@gmail.com>
2022-12-10 21:10:23 -0300

    test

--- dollar_none.py
+++
@@ -1,3 +0,0 @@
-class NoneException(Exception):
-    def __init__(self, message="Erro ao recuperar valor do dólar!."):
-        super().__init__(message)
--- list_empty.py
+++
@@ -1,3 +0,0 @@
-class EmptyException(Exception):
-    def __init__(self, message="Erro ao recuperar valores do select!."):
-        super().__init__(message)
